5cbd97c89f4d3f77fa12c9115265470b8d1c8910
Jior <JamesIoriattiII2023@u.northwestern.edu>
2023-04-12 20:17:52 -0500

    updated sidebar styling/functionality

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -13,9 +13,10 @@ export default function SideBar({font}){
             <motion.div 
                 className={styles.container + ' ' + font}
                 layout={true}
+                style={{boxShadow: state.scrollYPosition !=0 ? ' 8px 8px 5px -5px var(--themeColorLight)' : 'none'}}
                 // style={{height: songState.isTimerHit ? '100%' : 'calc(100% - 151px)'}}
-                animate={{width: isHovered? 300 : 75, originX: 0, height: songState.isTimerHit ? '100%' : 'calc(100% - 151px)'}}
-                transition={{width: {duration: 0.2, delay: isHovered? 0.4 : 0}, height:{duration: 0.2, delay: songState.isTimerHit ? 0.1 : 0}}}
+                animate={{width: (isHovered && state.scrollYPosition !=0)? 300 : 75, originX: 0, height: songState.isTimerHit ? '100%' : 'calc(100% - 151px)', translateX: state.scrollYPosition !=0 ? 0 : -100}}
+                transition={{width: {duration: 0.2, delay: isHovered ? 0.4 : 0}, height:{duration: 0.2, delay: songState.isTimerHit ? 0.1 : 0}, translateX:{duration: 0.2, delay: 0.8}}}
                 onMouseEnter={()=>setIsHovered(true)}
                 onMouseLeave={()=>setIsHovered(false)}
             >
@@ -23,35 +24,38 @@ export default function SideBar({font}){
                     <motion.div 
                         className={styles.background}
                         animate={{ opacity: state.scrollYPosition != 0 ? 1 : 0 }}
-                        style={{boxShadow: state.scrollYPosition !=0 ? ' 0 10px 10px 0 var(--themeColorLight)' : 'none'}}
                         transition={{ duration: 0.35 }}
                     >
                     </motion.div>
                     <div className={styles.iconContainer}>
-                        <div 
-                            className={styles.icon}
-                        >
-                            <img 
-                                src="/favorites-star.png" 
-                                alt="History" 
-                                className={styles.iconImage}
-                            />
+                        <div className={styles.itemWrapper}>
+                            <div 
+                                className={styles.icon}
+                            >
+                                <img 
+                                    src="/favorites-star.png" 
+                                    alt="History" 
+                                    className={styles.iconImage}
+                                />
+                            </div>
+                            <div className={styles.text}>Favorites</div>
                         </div>
-                        <div 
-                            className={styles.icon}
-                        >
-                            <img src="/eye.png" alt="History" className={styles.iconImage}/>
+                        <div className={styles.itemWrapper}>
+                            <div 
+                                className={styles.icon}
+                            >
+                                <img src="/eye.png" alt="History" className={styles.iconImage}/>
+                            </div>
+                            <div className={styles.text}>Recently Viewed Artists</div>
                         </div>
-                        <div 
-                            className={styles.icon}
-                        >
-                            <img src="/song-history.png" alt="History" className={styles.iconImage}/>
+                        <div className={styles.itemWrapper}>
+                            <div 
+                                className={styles.icon}
+                            >
+                                <img src="/song-history.png" alt="History" className={styles.iconImage}/>
+                            </div>
+                            <div className={styles.text}>History</div>
                         </div>
-                    </div>
-                    <div className={styles.textContainer}>
-                        <div className={styles.text}>Favorites</div>
-                        <div className={styles.text}>Recently Viewed Artists</div>
-                        <div className={styles.text}>History</div>
                     </div>
                 </div>
             </motion.div>
