79041b7e9f3d8c94a8b5a9bf4e89853e17d28301
Jior <JamesIoriattiII2023@u.northwestern.edu>
2023-04-17 17:14:53 -0500

    added Next-Auth and other associated packages for ability to log users in using Google provider, to add Spotify later

--- next.config.js
+++ next.config.js
@@ -16,6 +16,12 @@ const nextConfig = {
       hostname: 'i.scdn.co',
       port:'',
       pathname:'/image/**'
+    },
+    {
+      protocol: 'https',
+      hostname: 'lh3.googleusercontent.com',
+      port: '',
+      pathname:'/a/**'
     }
    ]
   },
