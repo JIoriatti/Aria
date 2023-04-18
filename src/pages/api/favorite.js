66e952b1d1bdcf90cf488f9855811100b7808fc7
Jior <JamesIoriattiII2023@u.northwestern.edu>
2023-04-18 14:45:23 -0500

    Added getFavorites handler

--- src/pages/api/favorite.js
+++ src/pages/api/favorite.js
@@ -1,14 +1,19 @@
-import { addFavorite } from "lib/prisma/favoriteController"
+import { addFavorite, getFavorites} from "lib/prisma/favoriteController"
 
 const favoriteHandler = async (req, res)=>{
   try{
+    const { songInfo } = JSON.parse(req.body);
+    const { userId } = JSON.parse(req.body);
+
     if(req.method === 'POST'){
-      const { songInfo } = JSON.parse(req.body);
-      const { userId } = JSON.parse(req.body);
-      console.log(JSON.parse(req.body))
-      const response = await addFavorite(songInfo, userId)
+      const response = await addFavorite(songInfo, userId);
+      console.log(response);
+      res.status(200).json(response);
+    }
+    if(req.method === 'GET'){
+      const response = await getFavorites(userId);
       console.log(response)
-      res.status(200).json(response)
+      res.status(200).json(response);
     } 
   }catch(err){
     console.log(err)
